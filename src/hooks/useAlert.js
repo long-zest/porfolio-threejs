16b41a50c8e5ca20cf159da6a23c9f5480d9eb1d
long-zest <zesta.long@gmail.com>
2023-12-04 16:08:33 +0700

    Add Alert component and About page

--- src/hooks/useAlert.js
+++ src/hooks/useAlert.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 const useAlert = () => {
-    const [alert, setAlert] = useState({ show: 'false', text: '', type: 'danger' })
+    const [alert, setAlert] = useState({ show: false, text: '', type: 'danger' })
 
     const showAlert = ({ text, type = 'danger' }) => setAlert({
         show: true,
